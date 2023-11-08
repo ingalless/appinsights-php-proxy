9f81aebad47bfcd17d64a1e8bcafcac8ccb66838
Jonny Ingall <jonny@mondago.com>
2023-11-08 14:21:08 +0000

    Start client

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,16 @@ struct HeartbeatBody {
     StreamId: String,
     Timestamp: String,
     Version: String,
-    Metrics: Vec<String>,
+    Metrics: Vec<Metric>,
+    Documents: serde_json::Value,
+}
+
+#[derive(Serialize, Deserialize)]
+#[allow(non_snake_case)]
+struct Metric {
+    Name: String,
+    Value: i32,
+    Weight: i32,
 }
 
 #[tokio::main]
@@ -25,7 +34,7 @@ async fn main() -> io::Result<()> {
 
     let app_insights_key: String =
         env::var(INSTRUMENTATION_KEY).expect("Var APPINSIGHTS_INSTRUMENTATIONKEY not set");
-    let appinsights = appinsights::TelemetryClient::new(app_insights_key.to_string());
+    let _appinsights = appinsights::TelemetryClient::new(app_insights_key.to_string());
 
     tokio::spawn(async {
         quickpulse_client().await;
@@ -36,8 +45,45 @@ async fn main() -> io::Result<()> {
 
 async fn quickpulse_client() {
     let mut is_collecting = false;
-    let mut interval = time::interval(time::Duration::from_secs(10));
-    let _metrics: Vec<String> = vec![];
+    let mut interval = time::interval(time::Duration::from_secs(5));
+
+    let _metrics: Vec<Metric> = vec![
+        Metric {
+            Name: r"\\ApplicationInsights\\Requests/Sec".to_string(),
+            Value: 1,
+            Weight: 1,
+        },
+        Metric {
+            Name: r"\\ApplicationInsights\\Request Duration".to_string(),
+            Value: 1,
+            Weight: 1,
+        },
+        Metric {
+            Name: r"\\ApplicationInsights\\Requests Failed/Sec".to_string(),
+            Value: 0,
+            Weight: 1,
+        },
+        Metric {
+            Name: r"\\ApplicationInsights\\Dependency Calls/Sec".to_string(),
+            Value: 0,
+            Weight: 1,
+        },
+        Metric {
+            Name: r"\\ApplicationInsights\\Dependency Call Duration".to_string(),
+            Value: 0,
+            Weight: 1,
+        },
+        Metric {
+            Name: r"\\Memory\\Committed Bytes".to_string(),
+            Value: 1234,
+            Weight: 1,
+        },
+        Metric {
+            Name: r"\\Processor(_Total)\\% Processor Time".to_string(),
+            Value: 0,
+            Weight: 1,
+        },
+    ];
     let _documents: Vec<String> = vec![];
 
     loop {
@@ -53,7 +99,91 @@ async fn quickpulse_client() {
 }
 
 async fn post() {
-    println!("Would have posted, but I don't know how");
+    println!("I'm gonna try post, but who knows if it will work");
+
+    let client = reqwest::Client::new();
+
+    let now = chrono::Utc::now().timestamp().to_string();
+
+    let app_insights_key = env::var(INSTRUMENTATION_KEY).unwrap();
+
+    let heartbeat_body = HeartbeatBody {
+        RoleName: "Web".to_owned(),
+        InstrumentationKey: app_insights_key.to_owned(),
+        InvariantVersion: "1".to_owned(),
+        MachineName: "dw0sdwk0005FD".to_owned(),
+        StreamId: "ff6f7facd28144de877018b12b4c382c".to_owned(),
+        Timestamp: format!(r"\/Date({})\/", now),
+        Version: "rust:0.0.1".to_owned(),
+        Documents: serde_json::Value::Null,
+        Metrics: get_metrics(),
+    };
+
+    let transmission_time = chrono::Utc::now()
+        .checked_add_signed(chrono::Duration::microseconds(62135596800000))
+        .unwrap()
+        .timestamp_nanos_opt()
+        .unwrap()
+        .to_string();
+    let mut heartbeat_headers = HeaderMap::new();
+    heartbeat_headers.insert("Expect", "100-continue".parse().unwrap());
+    heartbeat_headers.insert(
+        "x-ms-qps-transmission-time",
+        transmission_time.parse().unwrap(),
+    );
+
+    client
+        .post(format!(
+            "https://uksouth.livediagnostics.monitor.azure.com/QuickPulseService.svc/post?ikey={}",
+            app_insights_key
+        ))
+        .headers(heartbeat_headers.clone())
+        .body(serde_json::to_string(&heartbeat_body).unwrap())
+        .send()
+        .await
+        .unwrap();
+
+    println!("posted");
+}
+
+fn get_metrics() -> Vec<Metric> {
+    vec![
+        Metric {
+            Name: r"\\ApplicationInsights\\Requests/Sec".to_string(),
+            Value: 1,
+            Weight: 1,
+        },
+        Metric {
+            Name: r"\\ApplicationInsights\\Request Duration".to_string(),
+            Value: 1,
+            Weight: 1,
+        },
+        Metric {
+            Name: r"\\ApplicationInsights\\Requests Failed/Sec".to_string(),
+            Value: 0,
+            Weight: 1,
+        },
+        Metric {
+            Name: r"\\ApplicationInsights\\Dependency Calls/Sec".to_string(),
+            Value: 0,
+            Weight: 1,
+        },
+        Metric {
+            Name: r"\\ApplicationInsights\\Dependency Call Duration".to_string(),
+            Value: 0,
+            Weight: 1,
+        },
+        Metric {
+            Name: r"\\Memory\\Committed Bytes".to_string(),
+            Value: 1234,
+            Weight: 1,
+        },
+        Metric {
+            Name: r"\\Processor(_Total)\\% Processor Time".to_string(),
+            Value: 0,
+            Weight: 1,
+        },
+    ]
 }
 
 async fn ping() {
@@ -63,18 +193,32 @@ async fn ping() {
 
     let app_insights_key = env::var(INSTRUMENTATION_KEY).unwrap();
 
+    println!("{}", now);
+
     let heartbeat_body = HeartbeatBody {
-        RoleName: "app-teams-event-proxy".to_owned(),
+        RoleName: "Web".to_owned(),
         InstrumentationKey: app_insights_key.to_owned(),
         InvariantVersion: "1".to_owned(),
         MachineName: "dw0sdwk0005FD".to_owned(),
         StreamId: "ff6f7facd28144de877018b12b4c382c".to_owned(),
-        Timestamp: now,
+        Timestamp: format!(r"\/Date({})\/", now),
         Version: "rust:0.0.1".to_owned(),
-        Metrics: vec![r"\ApplicationInsights\Exceptions/Sec".to_owned()],
+        Documents: serde_json::Value::Null,
+        Metrics: get_metrics(),
     };
 
+    let transmission_time = chrono::Utc::now()
+        .checked_add_signed(chrono::Duration::microseconds(62135596800000))
+        .unwrap()
+        .timestamp_nanos_opt()
+        .unwrap()
+        .to_string();
+
     let mut heartbeat_headers = HeaderMap::new();
+    heartbeat_headers.insert(
+        "x-ms-qps-transmission-time",
+        transmission_time.parse().unwrap(),
+    );
     heartbeat_headers.insert(
         "x-ms-qps-stream-id",
         "ff6f7facd28144de877018b12b4c382c".parse().unwrap(),
@@ -82,7 +226,7 @@ async fn ping() {
     heartbeat_headers.insert("x-ms-qps-machine-name", "dw0sdwk0005FD".parse().unwrap());
     heartbeat_headers.insert(
         "x-ms-qps-role-name",
-        "app-teams-event-proxy".parse().unwrap(),
+        "Web".parse().unwrap(),
     );
     heartbeat_headers.insert("x-ms-qps-invariant-version", "1".parse().unwrap());
     heartbeat_headers.insert("Expect", "100-continue".parse().unwrap());
@@ -97,6 +241,8 @@ async fn ping() {
         .send()
         .await
         .unwrap();
+
+    println!("Pinged!");
 }
 
 // async fn process(client: &TelemetryClient, msg: SyslogMessage) {
